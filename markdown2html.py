03b659d46d6cc2ba7e75b849e7f412d8ecbd292a
ERIC UWIRINGIYIMANA <uwiringiyimanaericmax2000@gmail.com>
2024-11-03 23:01:21 +0200

    markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -13,4 +13,21 @@ if __name__ == "__main__":
         sys.stderr.write(f"Missing {sys.argv[1]}")
         sys.exit(1)
 
-    sys.exit(0)
+    converted_lines = []
+
+    with open(sys.argv[1], 'r') as rf:
+        for line in rf:
+            line = line.rstrip('\r\n')
+
+            count = 0
+            while count < len(line) and line[count] == '#':
+                count += 1
+            if 1 <= count <= 6 and line[count] == ' ':
+                content = line[count + 1:].strip()
+                converted_lines.append(f"<h{count}>{content}</h{count}>")
+            else:
+                converted_lines.append(f"<p>{line}</p>")
+
+    with open(sys.argv[2], 'a') as wf:
+        for line in converted_lines:
+            wf.write(line + "\n")
