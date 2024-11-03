33fdc54b6c24f4ef1cf79719a9d5677a93412289
ERIC UWIRINGIYIMANA <uwiringiyimanaericmax2000@gmail.com>
2024-11-03 21:56:24 +0200

    markdown

--- markdown2html.py
+++ markdown2html.py
@@ -4,11 +4,13 @@ import sys
 import os
 
 
-if len(sys.argv) < 3:
-    sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-    sys.exit(1)
-if not os.path.isfile(sys.argv[1]):
-    sys.stderr.write(f"Missing {sys.argv[1]}")
-    sys.exit(1)
+if __name__ == "__main__":
+    '''the number of arguments is less than 2'''
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        sys.exit(1)
+    if not os.path.isfile(sys.argv[1]):
+        sys.stderr.write(f"Missing {sys.argv[1]}")
+        sys.exit(1)
 
-sys.exit(0)
+    sys.exit(0)
